e2b76fb77ec790b38d5c985e89a285cb177049ab
jrclinton <jrclinton27@gmail.com>
2015-12-08 15:32:29 -0500

    Update jamesClinton.py

--- week6/jamesClinton.py
+++ week6/jamesClinton.py
@@ -16,45 +16,50 @@ def pullInData(fileName):
 # Will serve as the main mergesort function
 def MergeSort(someArray):
 	
-	# Check to see if the array is longer than 1
+    # Check to see if the array is longer than 1
     if len(someArray)>1:
-		# Finds the middle
+	# Finds the middle
         mid = len(someArray)//2
 		
-		# Splits the Left and Right halves
+	# Splits the Left and Right halves
         lefthalf = someArray[:mid]
         righthalf = someArray[mid:]
 
-		# Recursively mergeSorts
+	# Recursively mergeSorts
         MergeSort(lefthalf)
         MergeSort(righthalf)
 
-		# Lets create some variables for our while loops
+	# Lets create some variables for our while loops
         i=0
         j=0
         k=0
 		
-		# Will loop while i and j are less than leftHalf and rightHalf, respectively
-		# Combines the two arrays in sorted order
+	# Will loop while i and j are less than leftHalf and rightHalf, respectively
+	# Combines the two arrays in sorted order
         while i < len(lefthalf) and j < len(righthalf):
-                
+            
+            # Cast the strings as floats and check to see which is smaller
+            # If left is smaller it is put in the parent list first
             if float(lefthalf[i]) < float(righthalf[j]):
                 someArray[k]=lefthalf[i]
                 i=i+1
+                
+            # Else the right half is smaller and should be placed into the 
+            #  parent list first
             else:
                 someArray[k]=righthalf[j]
                 j=j+1
             k=k+1
 		
-		# Will fill out the list with the rest of leftHalf if ther are numbers left in items
-		# *Only occurs when rightHalf Empties
+	# Will fill out the list with the rest of leftHalf if ther are numbers left in items
+	# *Only occurs when rightHalf Empties
         while i < len(lefthalf):
             someArray[k]=lefthalf[i]
             i=i+1
             k=k+1
 		
-		# Will fill out the list with the rest of rightHalf if ther are numbers left in items
-		# *Only occurs when leftHalf Empties
+	# Will fill out the list with the rest of rightHalf if ther are numbers left in items
+	# *Only occurs when leftHalf Empties
         while j < len(righthalf):
             someArray[k]=righthalf[j]
             j=j+1
@@ -68,7 +73,8 @@ def main():
 	numbersList = pullInData(Filename)
 	MergeSort(numbersList)
 	output = open('output.txt', 'w')
-	#print (numbersList)
+	
+	# Loop the items into an output file
 	for item in numbersList:
 		output.write("%s\n" % item)
 	
